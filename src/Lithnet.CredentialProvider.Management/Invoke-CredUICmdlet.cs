a5c416ab41a1f3f482b83c224d338999b3a3cb50
Ryan Newington <ryan@lithiumblue.com>
2023-02-04 11:54:29 +1100

    Adds invoke-credui cmdlet

---
+++ src/Lithnet.CredentialProvider.Management/Invoke-CredUICmdlet.cs
@@ -0,0 +1,18 @@
+﻿using System.Management.Automation;
+using Lithnet.CredentialProvider.Management;
+
+namespace Lithnet.CredentialProvider.RegistrationTool
+{
+    [Cmdlet(VerbsLifecycle.Invoke, "CredUI")]
+    public class InvokeCredUICmdlet : PSCmdlet
+    {
+        public string Heading { get; set; } = "Login with CredUI";
+
+        public string Message { get; set; } = "Select your favorite credential provider";
+
+        protected override void EndProcessing()
+        {
+            CredUI.Prompt(Heading, Message);
+        }
+    }
+}
